2c9ab6cad558da5a75420c9cc35aabc7f79849b7
fattybufff <cmccann2@cps.edu>
2025-11-06 15:40:03 +0000

    start

--- README.md
+++
@@ -1,2 +0,0 @@
-# fairytale-game
-11/6 2025 fairytale game 
---
+++ script.js
@@ -0,0 +1,6 @@
+let button = document.querySelector("button");
+let words = document.querySelector("p");
+
+ button.addEventListener("click", function() {
+  words.innerHTML = "This is important!";
+ })
