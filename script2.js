c67875679b01e6c12fcda4bf1bb83d1cc24a2ae8
Carter <cmccann2@cps.edu>
2025-12-01 15:40:06 +0000

    jsndojna

--- script2.js
+++ script2.js
@@ -1,10 +1,3 @@
-// let keydown = document.querySelector("#keydown");
-
-// window.addEventListener("keydown", function (event) {
-//   header.innerHTML = `Event: ArrowDown (Key: ${event.key})`;
-//   window.location.replace("https://symmetrical-space-garbanzo-g4w65vg6xw44h9jj7-5500.app.github.dev/index.html");
-// });
-
 document.addEventListener("keydown", function(event) {
     if (event.key === "ArrowDown") {
         window.location.href = "index.html";
--- script3.js
+++ script3.js
@@ -1,6 +1,5 @@
 <button id="myButton">Go to Index</button>
 
-
 document.getElementById("myButton").addEventListener("dblclick", function () {
     window.location.href = "index.html";
 });
